9d72e3ee955b5efbc6f89a63eeff158f32cef0fa
parisa-rahmani <parisa.rahmani998@gmail.com>
2021-04-14 19:13:08 +0430

    add test

---
+++ src/containers/TodoList/TodoListtest.js
@@ -0,0 +1,18 @@
+// import React from 'react';
+// import { configure, shallow } from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-16';
+// import ListItems from '../../components/ListItems/ListItems';
+// import TodoList from './TodoList';
+
+// configure({ adapter: new Adapter() });
+
+// describe('<TodoList />', () => {
+//   let wrapper;
+//   beforeEach(() => {
+//     wrapper = shallow(<TodoList />);
+//   });
+
+//   it('should render <ListItems /> when recived initData', () => {
+//     expect(wrapper.find(ListItems).toHaveLength(1));
+//   });
+// });
